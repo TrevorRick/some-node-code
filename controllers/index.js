be73221391d9738f66dd2b01f475fdf30dd744f5
TrevorRick <TrevorRick@users.noreply.github.com>
2018-05-22 14:40:36 +0800

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,56 +1,41 @@
-/**
- * 模拟银行的接口，获取request API的response
- */
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const parseString = require('xml2js').parseString;
+// /**
+//  * 模拟银行的接口，获取request API的response
+//  */
+// const express = require('express');
+// const parseString = require('xml2js').parseString;
 
-const config = require('../config');
-const aesutil = require('../utils/aesutil');
+// const aesutil = require('../utils/aesutil');
 
-const key = config.key;
-const iv = config.iv;
+// let count = 0;
 
-const router = express.Router();
+// const router = express.Router();
 
-var parsePostBody = function (req, done) { 
-    let arr = [];
-    let chunks;
+// var parsePostBody = function (req, done) { 
+//     let arr = [];
+//     let chunks;
 
-    req.on('data', buffer => {
-        arr.push(buffer);
-    });
+//     req.on('data', buffer => {
+//         arr.push(buffer);
+//     });
 
-    req.on('end', () => {
-        chunks = Buffer.concat(arr);
-        done(chunks);
-    })
-}
+//     req.on('end', () => {
+//         chunks = Buffer.concat(arr);
+//         done(chunks);
+//     });
+// };
 
-router.post('/', function (req, res) {
-    parsePostBody(req, (chunks) => {
-        let body = chunks.toString();
+// router.post('/', function (req, res) {  
+//     parsePostBody(req, (chunks) => {
+//         let body = chunks.toString();
+//         let decryption_body = aesutil.decrypt(body); 
+//         parseString(decryption_body, function (err, result) {           
+//             let data = `<?xml version="1.0" encoding="GBK"?><root><head><retcode>000000</retcode><retmsg>签约对账数据查询成功</retmsg></head><body><totalpagenum>10</totalpagenum><nowpagenum>${count}</nowpagenum><pagerownum>10</pagerownum><opertype>1</opertype><name>张三</name><id>321111111111111113</id><phone>13888888883</phone><accno>626666666666663</accno><note1></note1><note2></note2></body></root>`;
+//             res.writeHead(200, {'Content-Type': 'application/xml'});
+//             res.end(data);
+//             count++;    
+//         }); 
+//     });
+// });
 
-        let hashkey = aesutil.maphash(key);
-        let hashiv = aesutil.maphash(iv, config.addalgorithm);
-        let decryption_body = aesutil.decryption(body, hashkey, hashiv); 
-        parseString(decryption_body, function (err, result) { // result-->解密后的xml文件转换成的JSON Object
-            let transcode = result.root.head[0].transcode;  //根据transcode执行不同的逻辑代码
-            if ( transcode == "T0001") {
-                // 代扣签约
-                res.writeHead(200, {'Content-Type': 'application/xml'});
-                res.end("T0001 request");
-            } else if (transcode == "T0002") {
-                res.writeHead(200, {'Content-Type': 'application/xml'});
-                res.end("T0001 request");
-            } else if (transcode == "T0005"){
-                let data = '<?xml version="1.0" encoding="GBK"?><root><head><retcode>000000</retcode><retmsg>签约对账数据查询成功</retmsg></head><body><totalpage>10</totalpage><nowpage>0</nowpage><pagerownum>20</pagerownum><opertype>1</opertype><name>张三</name><id>321111111111111113</id><phone>13888888883</phone><accno>626666666666663</accno><note1></note1><note2></note2></body></root>'
-                res.writeHead(200, {'Content-Type': 'application/xml'});
-                res.end(data);
-            };            
-        })       
-    })
-})
+// module.exports = router;
 
-module.exports = router;
