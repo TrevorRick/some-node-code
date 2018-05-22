7f2bb967b509bb7780bf12ad64feebe5c7d09b4c
TrevorRick <TrevorRick@users.noreply.github.com>
2018-05-22 14:37:23 +0800

    Update testmongo.js

--- utils/testmongo.js
+++ utils/testmongo.js
@@ -1,4 +1,4 @@
-const mongoquery = require('./mongoquery');
+// const mongoquery = require('./mongoquery');
 
-mongoquery.identify('孙曙光', '372301196807314111', '13581159718', '6212261613008973702');
+// mongoquery.identify('xxx', 'xxxx', 'xxx', 'xx');
 
