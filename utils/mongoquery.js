4e0af57c518ad3251f24dd904ac31af8569a49aa
TrevorRick <TrevorRick@users.noreply.github.com>
2018-05-22 14:35:10 +0800

    Update mongoquery.js

--- utils/mongoquery.js
+++ utils/mongoquery.js
@@ -1,27 +1,27 @@
-const mongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-const config = require('../config');
+// const mongoClient = require('mongodb').MongoClient;
+// const assert = require('assert');
+// const config = require('../config');
 
-const url = 'mongodb://localhost:27017';
-const dbName = 'stock';
+// const url = 'mongodb://localhost:27017';
+// const dbName = 'stock';
 
 
-const mongoquery = module.exports = {};
+// const mongoquery = module.exports = {};
 
-mongoquery.identify = function (name, id, phone, accno) {
-    mongoClient.connect(url, function (err, client) {
-        assert.equal(null, err);
-        console.log("Connected successfully to server");
+// mongoquery.identify = function (name, id, phone, accno) {
+//     mongoClient.connect(url, function (err, client) {
+//         assert.equal(null, err);
+//         console.log("Connected successfully to server");
         
-        const collection = client.db(dbName).find({
-            name: name,
-            id: id,
-            phone: phone,
-            accno: accno
-        }).toArray(function (err, docs) {
-            assert.equal(err, null);
-            return true;
-        })
+//         const collection = client.db(dbName).find({
+//             name: name,
+//             id: id,
+//             phone: phone,
+//             accno: accno
+//         }).toArray(function (err, docs) {
+//             assert.equal(err, null);
+//             return true;
+//         })
     //    findDocuments(db, name, function (docs) {
     //         callback((docs[0].id == id && docs[0].phone == phone && docs[0].accno == accno));
     //         client.close();
